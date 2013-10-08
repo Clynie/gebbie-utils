ed1540bdee810c6bf143c9fec5b131324394dd5c
John Gebbie <john.gebbie@gmail.com>
2013-10-08 13:06:05 -0700

    clean header

--- matlab/smooth_gaussian.m
+++ matlab/smooth_gaussian.m
@@ -1,4 +1,20 @@
 function [yi] = smooth_gaussian(x,y,xi,xsigma)
+% Simple Gaussian smoothing, interpolation, and extrapolation routine from
+% irregularly smapled input data points to any output data points.
+% 1-dimensional.
+%
+% INPUT x : x-coordinate of input data
+%
+% INPUT y : y-coordinate (value) of input data
+%
+% INPUT xi : x-coordinate to interpolate y-value at
+%
+% INPUT xsigma : sigma (width) of Gaussian
+%
+% OUTPUT yi : interpolated y-value. same dimensions as xi.
+%
+% Author: John Gebbie
+% Modify Date: 2013 Oct 08
 
 %normpdf = @(x,mu,sigma) exp(-(x-mu).^2./(2.*sigma.^2));
 lognormpdf = @(x,mu,sigma) -(x-mu).^2./(2.*sigma.^2);
